dab4f9258a74bc110ec84bd3de4b13c85b26b614
larginator <larginator@gmail.com>
2024-05-09 15:02:19 +0600

    Add Main class

--- srs/main/java/com/andrey/Main.java
+++ srs/main/java/com/andrey/Main.java
@@ -1,7 +1,8 @@
 package srs.main.java.com.andrey;
 
 public class Main {
+    private static final String HELLO_MESSAGE = "Hello World!";
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(HELLO_MESSAGE);
     }
 }
